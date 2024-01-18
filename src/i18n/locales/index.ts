25e44ab60446a9b128c17daf0d2ad8cb527d936a
Dionei Bianchati <dion.biancha@gmail.com>
2024-01-18 10:18:26 -0300

    fix

--- src/i18n/locales/index.ts
+++ src/i18n/locales/index.ts
@@ -1,7 +1,9 @@
 import ptBrTranslations from "./pt-br.json";
 import enUsTranslations from "./en-us.json";
 
-export default {
+const translations: Record<string, any> = {
   "pt-BR": ptBrTranslations,
   "en-US": enUsTranslations,
 };
+
+export default translations;
