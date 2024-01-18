eba99eee35d090d623d7db7e321eae81d5ffd003
Dionei Bianchati <dion.biancha@gmail.com>
2024-01-18 10:56:47 -0300

    feat create and delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import { Container, Divider, Stack } from "@chakra-ui/react";
+import { Box, Button, Container, Divider, Stack } from "@chakra-ui/react";
 import "./i18n";
 import Header from "./features/header";
 import SelectLogo from "./features/SelectLogo";
@@ -11,16 +11,72 @@ import Date from "./features/Date";
 import Information from "./features/Information";
 import TextInput from "./components/TextInput";
 import QRCode from "react-qr-code";
+import { AddIcon } from "@chakra-ui/icons";
+import { useTranslation } from "react-i18next";
 
-function App() {
-  const { qrCodeLink } = usePreview();
+function ContentArea() {
   useEffect(() => {
     serviceWorkerRegistration.register();
   }, []);
 
   return (
-    <>
-      <Container maxW="1200px" padding="30px">
+    <Stack direction={"row"} justifyContent={"space-between"}>
+      <Stack
+        sx={{
+          backgroundColor: "#e2e8f01c",
+          width: "350px",
+          padding: "15px",
+        }}
+        direction={"column"}
+      >
+        <Date />
+        <Location />
+      </Stack>
+      <Stack
+        sx={{
+          width: "100%",
+          paddingLeft: "20px",
+        }}
+        direction={"column"}
+      >
+        <Information />
+        <TextInput
+          title="Politica de Cancelamentos"
+          placeholder="Digite suas politica de cancelamentos"
+          emptyText="Nenhum dado encontrada"
+        />
+        <TextInput
+          title="Observações"
+          placeholder="Digite suas obervações"
+          emptyText="Nenhuma observação encontrada"
+        />
+      </Stack>
+    </Stack>
+  );
+}
+
+function App() {
+  const { t } = useTranslation();
+  const { qrCodeLink } = usePreview();
+  const [additionalAreas, setAdditionalAreas] = useState<
+    Record<string, unknown>[]
+  >([]);
+
+  const addAdditionalArea = () => {
+    setAdditionalAreas((prevAreas) => [...prevAreas, {}]);
+  };
+
+  const removeAdditionalArea = (index: number) => {
+    setAdditionalAreas((prevAreas) => prevAreas.filter((_, i) => i !== index));
+  };
+
+  useEffect(() => {
+    serviceWorkerRegistration.register();
+  }, []);
+
+  return (
+    <Stack direction={"column"} padding="30px" alignItems={"center"}>
+      <Container maxW="1200px">
         <Header />
       </Container>
       <Container id="pdf" maxW="1200px" paddingX="30px">
@@ -33,40 +89,54 @@ function App() {
           {qrCodeLink && <QRCode size={150} value={qrCodeLink} />}
         </Stack>
         <Divider sx={{ marginY: "30px" }} />
-        <Stack direction={"row"} justifyContent={"space-between"}>
-          <Stack
-            sx={{
-              backgroundColor: "#e2e8f01c",
-              width: "350px",
-              padding: "15px",
-            }}
-            direction={"column"}
-          >
-            <Date />
-            <Location />
-          </Stack>
-          <Stack
-            sx={{
-              width: "100%",
-              paddingLeft: "20px",
-            }}
-            direction={"column"}
-          >
-            <Information />
-            <TextInput
-              title="Politica de Cancelamentos"
-              placeholder="Digite suas politica de cancelamentos"
-              emptyText="Nenhum dado encontrada"
-            />
-            <TextInput
-              title="Observações"
-              placeholder="Digite suas obervações"
-              emptyText="Nenhuma observação encontrada"
-            />
-          </Stack>
-        </Stack>
+        <ContentArea />
       </Container>
-    </>
+      {additionalAreas.map((_, index) => (
+        <Container key={index} id={`pdf${index}`} maxW="1200px" paddingX="30px">
+          <Divider sx={{ marginY: "30px" }} />
+          <ContentArea />
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              marginTop: "20px",
+              border: "1px dashed",
+              width: "100%",
+              maxWidth: "1140px",
+              padding: "20px",
+              borderRadius: "5px",
+              fontSize: "13px",
+            }}
+          >
+            <Button
+              onClick={() => removeAdditionalArea(index)}
+              colorScheme="red"
+            >
+              Excluir
+            </Button>
+          </Box>
+        </Container>
+      ))}
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: "20px",
+          border: "1px dashed",
+          width: "100%",
+          maxWidth: "1140px",
+          padding: "20px",
+          borderRadius: "5px",
+          fontSize: "13px",
+        }}
+      >
+        <Button size={"lg"} onClick={addAdditionalArea} colorScheme="gray">
+          <AddIcon />
+        </Button>
+      </Box>
+    </Stack>
   );
 }
 
